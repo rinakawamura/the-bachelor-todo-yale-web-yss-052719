73e17c5864661a4948273dc3f72e83807691d04d
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-17 16:00:33 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,60 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = nil
+  data.each do |season_data, contestants|
+    if season_data == season
+      contestants.each do |contestant_hash|
+        if contestant_hash["status"] == "Winner"
+          winner = contestant_hash["name"]
+        end
+      end
+    end
+  end
+  return winner.split(" ").first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_data, contestants|
+    contestants.each do |contestant_hash|
+      if contestant_hash["occupation"] == occupation
+        return contestant_hash["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season_data, contestants|
+    contestants.each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_data, contestants|
+    contestants.each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        return contestant_hash["occupation"]
+      end
+    end
+  end
 end
 
+
 def get_average_age_for_season(data, season)
-  # code here
+  total_age = 0
+  num_contestants = 0
+  data.each do |season_data, contestants|
+    if season_data == season
+      contestants.each do |contestant_hash|
+        total_age += contestant_hash["age"].to_f
+        num_contestants += 1
+      end
+    end
+  end
+  return (total_age/num_contestants).round
 end
