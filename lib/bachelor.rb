6ad2c4f18445016fa6ffc78b0df60dc65f1ac724
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-17 15:39:52 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -413,11 +413,9 @@ def get_first_name_of_season_winner(data, season)
         if contestant_hash[:status] == "Winner"
           winner = contestant_hash[:name]
         end
-        #binding.pry
       end
     end
   end
-  puts winner.split(" ").first
   return winner.split(" ").first
 end
 
