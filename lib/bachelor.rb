f4550a0f350478ad4bb09f749908f8afdd2795ec
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-17 15:55:50 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -39,12 +39,13 @@ end
 def get_occupation(data, hometown)
   data.each do |season_data, contestants|
     contestants.each do |contestant_hash|
-      if contestant_hash["occupation"] == occupation
+      if contestant_hash["hometown"] == hometown
         return contestant_hash["occupation"]
       end
     end
   end
 end
+
 
 def get_average_age_for_season(data, season)
   # code here
